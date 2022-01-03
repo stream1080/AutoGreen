b00795af9bd4733705cb69265f4312a480caecac
stream1080 <fmxhax638@foxmail.com>
2022-01-03 11:04:36 +0800

    feat: perfect the script

--- Auto.bat
+++ Auto.bat
@@ -3,7 +3,5 @@
 
 echo %date:~0,10% %time%^>>CommitTime.txt
 git add .
-git commit -m "A commit a day keeps the girlfriend away."
+git commit -m "feat: perfect the script"
 git push -u origin master
-
-echo I'm Stronger...
--- linux-auto.sh
+++ linux-auto.sh
@@ -2,5 +2,5 @@
 cd /home/AutoGreen/
 echo $(date)>>CommitTime.txt
 /usr/local/git/bin/git add .
-/usr/local/git/bin/git commit -m "Everyday commit"
+/usr/local/git/bin/git commit -m "feat: update the time"
 /usr/local/git/bin/git push -u origin main
