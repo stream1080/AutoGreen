2b061f3a392ac64dc7aa1da6a73d849f21a1e126
stream1080 <fmxhax638@foxmail.com>
2022-01-03 10:19:41 +0800

    A commit a day keeps

--- linux-auto.sh
+++ linux-auto.sh
@@ -3,6 +3,6 @@ cd /home/AutoGreen/
 echo $(date)>>CommitTime.txt
 git add .
 git commit -m "A commit a day keeps the girlfriend away."
-git push -u origin master
+git push -u origin main
 echo I'M Stronger..
 
